9f034720587e01e4ac71b83f0eeccc0f7e2f9c74
Claire Michailovsky <cmi001@un-ihe.org>
2021-06-10 14:08:53 +0200

    Revert "update paths for sheet2"
    This reverts commit 7d6e4c20cd210386aedfb923d8f8eba9018665db.

--- paths.py
+++ paths.py
@@ -11,7 +11,7 @@ def get_path(name):
     install_path = os.path.dirname(WA_Hyperloop.__file__)
     paths = {
              'sheet1_svg':  os.path.join(install_path, "svg\sheet_1_v2.svg"),
-             'sheet2_svg':  os.path.join(install_path, "svg\sheet_2_national.svg"),
+             'sheet2_svg':  os.path.join(install_path, "svg\sheet_2.svg"),
              'sheet3_svg': os.path.join(install_path, "svg\sheet_3_national.svg"),
              'sheet4_1_svg':  os.path.join(install_path, "svg\sheet_4_part1.svg"),
              'sheet4_2_svg':  os.path.join(install_path, "svg\sheet_4_part2.svg"),
